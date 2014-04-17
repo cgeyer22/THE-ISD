e8cc996ba1aa22cf6109c65cd274ec4099baa217
Charles <texanbodybuilder@yahoo.com>
2014-04-16 20:51:26 -0400

    Ignore
    This thing is being dumb, no SIGNIFICANT changes were made

--- ExercisePage.aspx.vb
+++ ExercisePage.aspx.vb
@@ -3,7 +3,7 @@ Partial Class ExercisePage_Default
     Inherits System.Web.UI.Page
     Dim connectionString As String = "Data Source=SIMON;Initial Catalog=BetaSYS39414;Persist Security Info=True;User ID=sking;Password=pxdrlgyu"
 
-    
+
 
 
     Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
